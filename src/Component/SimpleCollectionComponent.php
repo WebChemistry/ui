9ae06b6524f0f3d407a1dc1713dd0ac5989669eb
MartkCz <martk494@gmail.com>
2024-06-03 12:48:08 +0200

    SimpleCollectionComponent: add features

--- src/Component/SimpleCollectionComponent.php
+++ src/Component/SimpleCollectionComponent.php
@@ -6,24 +6,69 @@ use Nette\Application\UI\Control;
 use Nette\Bridges\ApplicationLatte\Template;
 use Nette\ComponentModel\IComponent;
 use LogicException;
+use Nette\Utils\Html;
 
 final class SimpleCollectionComponent extends Control
 {
+
+	private bool $lazyLoading = false;
 
 	private bool $isAnchored = false;
 
 	private bool $empty = true;
 
-	public function __construct()
+	private ?Html $wrapper = null;
+
+	/**
+	 * @param array<string, IComponent|null> $components
+	 */
+	public function __construct(array $components = [], Html|string|null $wrapper = null)
 	{
 		$this->onAnchor[] = function (): void {
 			$this->isAnchored = true;
 		};
+
+		foreach ($components as $name => $component) {
+			if ($component) {
+				$this->addComponent($component, $name);
+			}
+		}
+
+		$this->setWrapper($wrapper);
+	}
+
+	public function allowLazyLoading(): self
+	{
+		$this->lazyLoading = true;
+
+		return $this;
 	}
 
 	public function isEmpty(): bool
 	{
 		return $this->empty;
+	}
+
+	public function clear(): void
+	{
+		$this->empty = true;
+
+		foreach ($this->getComponents() as $component) {
+			$this->removeComponent($component);
+		}
+	}
+
+	public function prependComponent(IComponent $component, ?string $name): static
+	{
+		$components = iterator_to_array($this->getComponents());
+
+		if (!$components) {
+			$insertBefore = null;
+		} else {
+			$insertBefore = array_key_first($components);
+		}
+
+		return $this->addComponent($component, $name, $insertBefore);
 	}
 
 	public function addComponent(IComponent $component, ?string $name, string $insertBefore = null): static
@@ -37,6 +82,29 @@ final class SimpleCollectionComponent extends Control
 		return parent::addComponent($component, $name, $insertBefore);
 	}
 
+	/**
+	 * @param array<string, IComponent|null> $components
+	 */
+	public function addGroup(array $components, string $name, Html|string|null $wrapper = null, string $insertBefore = null): SimpleCollectionComponent
+	{
+		$collection = new self($components, $wrapper);
+
+		$this->addComponent($collection, $name);
+
+		return $collection;
+	}
+
+	public function setWrapper(Html|string|null $wrapper): self
+	{
+		if (is_string($wrapper)) {
+			$this->wrapper = Html::el('div', ['class' => $wrapper]);
+		} else {
+			$this->wrapper = $wrapper;
+		}
+
+		return $this;
+	}
+
 	public function render(): void
 	{
 		/** @var Template $template */
@@ -44,7 +112,24 @@ final class SimpleCollectionComponent extends Control
 
 		$template->render(__DIR__ . '/templates/simpleCollection.latte', [
 			'components' => $this->getComponents(),
+			'wrapper' => $this->wrapper,
 		]);
+	}
+
+	public function renderLazyLink(): void
+	{
+		if (!$this->lazyLoading) {
+			throw new LogicException('Lazy loading is not allowed.');
+		}
+
+		echo $this->link('load!');
+	}
+
+	public function handleLoad(): void
+	{
+		$this->render();
+
+		$this->getPresenter()->terminate();
 	}
 
 }
